0844b541e347d037ce32b8b4c9314d64d18c31df
bonbongiraffe <francescowai@yahoo.com>
2023-08-08 11:05:51 -0400

    authentication wip

--- client/src/components/Authentication.js
+++ client/src/components/Authentication.js
@@ -2,9 +2,26 @@ import { useEffect,useState } from "React"
 
 function Authentication(){
     const [ isLogin, setIsLogin ] = useState(False)
+    const [ formData, setFormData ] = useState({username:"",password:""})
+
+    function handleSubmit(){
+
+    }
 
     return(
-        <div></div>
+        <div class="authentication">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">username</label>
+                    <input 
+                        onChange= {handleName}
+                        type="text"
+                        username= "username"
+                        placeholder="username"
+                        className="input-text"
+                        value={formData.name}
+                    ></input>
+            </form>
+        </div>
     )
 }
 
