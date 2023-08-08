3bbc2a5a03c8b3313f737088badc3f596b5d3512
bonbongiraffe <francescowai@yahoo.com>
2023-08-08 10:52:31 -0400

    start authentication

---
+++ client/src/components/Authentication.js
@@ -0,0 +1,11 @@
+import { useEffect,useState } from "React"
+
+function Authentication(){
+    const [ isLogin, setIsLogin ] = useState(False)
+
+    return(
+        <div></div>
+    )
+}
+
+export default Authentication
