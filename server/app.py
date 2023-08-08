01607d6b0b34332557218ff068e257eb4e493d4b
bonbongiraffe <francescowai@yahoo.com>
2023-08-08 16:34:58 -0400

    auth wip

--- server/app.py
+++ server/app.py
@@ -54,36 +54,42 @@ class Hikes(Resource):
         except ValueError as v_error:
             return make_response({'errors':[v_error]},400)
 
-class Login(Resource):
-    def post(self):
-        data = request.get_json()
-        user = User.query.filter_by(username = data["username"]).first()
-        if not user:
-            return make_response({"error": "User not found"}, 400)
-        elif user.authenticate(data["password"]):
-            return make_response(user.to_dict(only=('username','id')), 200)
-        else:
-            return make_response({"error": "Incorrect password"}, 400)
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username = data["username"]).first()
+    if not user:
+        return make_response({"error": "User not found"}, 400)
+    elif user.authenticate(data["password"]):
+        return make_response(user.to_dict(only=('username','id')), 200)
+    else:
+        return make_response({"error": "Incorrect password"}, 400)
 
-class Signup(Resource):
-    def post(self):
-        data = request.get_json()
-        try:
-            new_user = User(
-                username=data["username"],
-                password_hash=data["password"]
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            return make_response(new_user.to_dict(only=('username','id')), 201)
-        except ValueError as v_error:
-            return make_response({"error":[v_error]}, 400)    
+@app.route('/signup', methods=["POST"])
+def signup():
+    data = request.get_json()
+    try:
+        new_user = User(
+            username=data["username"],
+            password_hash=data["password"]
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return make_response(new_user.to_dict(only=('username','id')), 201)
+    except ValueError as v_error:
+        return make_response({"error":[v_error]}, 400)    
+
+@app.route('/authorized', methods=["GET"])
+def authorized():
+    try:
+        user = User.query.filter_by(id=session["user_id"]).first()
+        return make_response( user.to_dict(only=('username','id')), 200)
+    except:
+        return make_response({"error": "Please log in or sign up"}, 401)
 
 api.add_resource(Users,'/users')
 api.add_resource(Trails,'/trails')
 api.add_resource(Hikes,'/hikes')
-api.add_resource(Signup,'/signup')
-api.add_resource(Login,'/login')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
