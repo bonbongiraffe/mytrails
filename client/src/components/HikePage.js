b1f47221906f8893971438e6b58d90e2f79fc113
Shanley Caswell <shanleycaswell@gmail.com>
2023-08-09 11:26:04 -0400

    beginning of hike page

--- client/src/components/HikePage.js
+++ client/src/components/HikePage.js
@@ -1,8 +1,19 @@
 import HikeCard from "./HikeCard"
 import HikeForm from "./HikeForm"
 import NavBar from "./NavBar"
+import { Card} from "semantic-ui-react"
+import { React, useState, useEffect }from "react"
+
+
 
 function HikePage(){
+    const [hikes, setHikes] = useState([])
+
+useEffect(() => {
+    fetch('/hikes')
+    .then(r => r.json())
+    .then(data => setHikes(data))
+})
     return (
         <div>
             <HikeCard />
