71eae221299e68ab4f75473b978eb0e88f7c05e7
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-08-10 17:20:43 -0400

    try to style scrollbar

--- client/src/components/HikePage.js
+++ client/src/components/HikePage.js
@@ -22,7 +22,8 @@ function HikePage({user}){
         document.title="My Trails | My Hikes"
     }, [])
 
-    const eachHike = hikes.filter((hike) => hike.user_id === user.id).map(filteredHike => {
+    // "?" = optional chaining
+    const eachHike = hikes.filter((hike) => hike.user_id === user?.id).map(filteredHike => {
         return (
             <HikeCard   key = {filteredHike.id}
                         trailName={filteredHike.trail.name}
