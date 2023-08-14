04c2ebcb3d6d9901c15f8fe9cba3de0ddeba7329
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-08-14 13:07:30 -0400

    requirements.txt

--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db, bcrypt
+from server.config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
